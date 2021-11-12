d1bffdef8f12e8f640fb205e13e8f8cb176faba2
tgmichel <telmo@purestake.com>
2021-11-12 10:42:04 +0100

    `tracing-step` feature for `evm-runtime`
    (cherry picked from commit 5fdaf44cd380766af02ae1798137ccd573554556)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -40,6 +40,7 @@ use alloc::rc::Rc;
 macro_rules! step {
 	( $self:expr, $handler:expr, $return:tt $($err:path)?; $($ok:path)? ) => ({
 		if let Some((opcode, stack)) = $self.machine.inspect() {
+			#[cfg(feature = "tracing-step")]
 			event!(Step {
 				context: &$self.context,
 				opcode,
